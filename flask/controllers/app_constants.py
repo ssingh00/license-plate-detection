124857a29c8868408fffdbe57b60a72b4892827f
Sandeep Singh <sandeep.it84@gmail.com>
2021-06-23 06:17:52 +0000

    update requirements and file paths

--- flask/controllers/app_constants.py
+++ flask/controllers/app_constants.py
@@ -1,5 +1,7 @@
 project_id = "qwiklabs-gcp-04-5e7d5c24e704"
 model_id = "IOD4232158793774923776"
 file_path = "license_car.jpg"
-UPLOAD_FOLDER = r'C:\Users\Anoop Talwar\Desktop\ASL Project\license-plate-detection\flask\resources\received_images'
-CROPPED_FOLDER = r'C:\Users\Anoop Talwar\Desktop\ASL Project\license-plate-detection\flask\resources\cropped_images'
+# UPLOAD_FOLDER = r'C:\Users\Anoop Talwar\Desktop\ASL Project\license-plate-detection\flask\resources\received_images'
+# CROPPED_FOLDER = r'C:\Users\Anoop Talwar\Desktop\ASL Project\license-plate-detection\flask\resources\cropped_images'
+UPLOAD_FOLDER = r'/resources/received_images'
+CROPPED_FOLDER = r'/resources/cropped_images'
