8226835d4612229d9947df8bef3d225998c91fb0
anooptalwar <anoop.talwar@mavenwave.com>
2021-06-24 15:00:01 +0530

    Updating New Model_id and project_id

--- flask/controllers/app_constants.py
+++ flask/controllers/app_constants.py
@@ -1,5 +1,5 @@
-project_id = "qwiklabs-gcp-04-5e7d5c24e704"
-model_id = "IOD4232158793774923776"
+project_id = "qwiklabs-gcp-04-568443837277"
+model_id = "IOD8721121722356465664"
 file_path = "license_car.jpg"
 # UPLOAD_FOLDER = r'C:\Users\Anoop Talwar\Desktop\ASL Project\license-plate-detection\flask\resources\received_images'
 # CROPPED_FOLDER = r'C:\Users\Anoop Talwar\Desktop\ASL Project\license-plate-detection\flask\resources\cropped_images'
